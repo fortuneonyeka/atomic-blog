dde5017222b53f11e7209a9cadd0df966adc32d1
fortune onyeka <ihedfortune@gmail.com>
2025-01-22 14:20:06 +0100

    divide app into components, manage state with simple contextAPI

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import Results from './Results';
+import SearchPosts from './SearchPosts';
+import { PostContext } from '../context/PostContext';
+
+const Header = () => {
+  const {onClearPosts} = useContext(PostContext)
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Atomic Blog
+      </h1>
+      <div>
+        <Results />
+        <SearchPosts
+          
+         
+        />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header
+
+
---
+++ src/components/SearchPosts.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { PostContext } from '../context/PostContext';
+
+const SearchPosts = () => {
+  const {searchQuery, setSearchQuery} = useContext(PostContext)
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
+export default SearchPosts
+
+
+
