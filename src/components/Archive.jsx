c8372a5bf4a585df1d94fe7f9c89b68654ed5f2c
fortune onyeka <ihedfortune@gmail.com>
2025-02-21 10:04:07 -0400

    momoiaze archive component

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -1,38 +1,41 @@
-import React, { useState } from 'react'
+import React, { memo, useState } from 'react'
 import { createRandomPost, usePostContext } from '../context/PostContext';
 
-const Archive = () => {
-  const [posts] = useState(() =>
-    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
-  );
 
-  const [showArchive, setShowArchive] = useState(false);
+  const Archive = memo(function Archive(archiveOptions) {
+    const [posts] = useState(() =>
+      // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+      Array.from({ length: 10000 }, () => createRandomPost())
+    );
+  
+    const [showArchive, setShowArchive] = useState(false);
+  
+    const {onAddPost} = usePostContext()
+  
+    return (
+      <aside>
+        <h2>Post archive</h2>
+        <button onClick={() => setShowArchive((s) => !s)}>
+          {showArchive ? "Hide archive posts" : "Show archive posts"}
+        </button>
+  
+        {showArchive && (
+          <ul>
+            {posts.map((post, i) => (
+              <li key={i}>
+                <p>
+                  <strong>{post.title}:</strong> {post.body}
+                </p>
+                <button onClick={() => onAddPost(post)}>Add as new post</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </aside>
+    );
+  })
+  
 
-  const {onAddPost} = usePostContext()
-
-  return (
-    <aside>
-      <h2>Post archive</h2>
-      <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? "Hide archive posts" : "Show archive posts"}
-      </button>
-
-      {showArchive && (
-        <ul>
-          {posts.map((post, i) => (
-            <li key={i}>
-              <p>
-                <strong>{post.title}:</strong> {post.body}
-              </p>
-              <button onClick={() => onAddPost(post)}>Add as new post</button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </aside>
-  );
-}
 
 export default Archive
 
