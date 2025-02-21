19075300b57aa502585bc2031261a5badca45efc
fortune onyeka <ihedfortune@gmail.com>
2025-02-21 10:48:33 -0400

    App is now only responsible for wrapping everything in PostProvider

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Archive from "./components/Archive";
 import Footer from "./components/Footer";
-import { PostProvider } from "./context/PostContext";
+import { PostProvider, usePostContext } from "./context/PostContext";
 import DarkModeToggle from "./components/DarkModeToggle";
+import { useMemo } from "react";
 
 function App() {
+
   return (
     <PostProvider>
-      <section>
-        <DarkModeToggle />
-
-        <Header />
-        <Main />
-        <Archive />
-        <Footer />
-      </section>
+      <AppContent />
     </PostProvider>
   );
 }
 
-<Footer />;
+  function AppContent() {
+    const { posts, searchedPosts } = usePostContext();
+  
+    const archiveOptions = useMemo(() => ({
+      show: false,
+      title: `Posts archive in addition to ${posts.length} main posts`
+    }), [posts.length]);
+  
+    return (
+      <section>
+        <DarkModeToggle />
+        <Header />
+        <Main />
+        <Archive archiveOptions={archiveOptions} />
+        <Footer />
+      </section>
+    );
+  }
 
 export default App;
